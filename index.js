9e8bfc0984224b0f8c0f46e2d9ab5e82dcd85f3c
Jahnvi <jahnvi18patel@gmail.com>
2021-03-24 14:14:58 +0000

    index route for places

--- index.js
+++ index.js
@@ -26,3 +26,4 @@ const startServer = async () => {
   }
 }
 startServer()
+
--- models/place.js
+++ models/place.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
-const placesSchema = new mongoose.Schema({
+const placeSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   longitude: { type: Number, required: true },
   latitude: { type: Number, required: true },
-  icon: { type: String, required: true }
+  icon: { type: String }
 })
 
-export default mongoose.model('Places', placesSchema)
+export default mongoose.model('Place', placeSchema)
