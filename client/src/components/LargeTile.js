c8d72902520961ef4edbd3af8aef57239fb5384e
Jahnvi <jahnvi18patel@gmail.com>
2021-03-28 00:43:25 +0000

    hover working

--- client/src/components/LargeTile.js
+++ client/src/components/LargeTile.js
@@ -13,13 +13,14 @@ const LargeTile = ({ _id, image, name, description, season, duration, price }) =
         //   { backgrroundPosition: 'center' }
         // }
         key={_id}>
+        {/* <div className="package-flip"> */}
         <div className="packages-li-container">
           <img src={image} className="package-background" key={_id} />
           <div className="left">
             <div className="package-title">
               {name}
             </div>
-            <div className="right">
+            <div className="right" >
               <ul className="package-details-ul">
                 <li className="package-details">
                   Duration: {duration} days
@@ -37,6 +38,7 @@ const LargeTile = ({ _id, image, name, description, season, duration, price }) =
             <p className="tile-description">{description}</p>
           </div>
         </div>
+        {/* </div> */}
       </li >
     </>
   )
