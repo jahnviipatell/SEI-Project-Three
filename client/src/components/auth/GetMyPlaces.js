ef9a68aa2be708c065ea308eae4aaca872385f1a
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-03-31 15:24:39 +0100

    added state and content to GetMyPlaces

--- client/src/components/auth/GetMyPlaces.js
+++ client/src/components/auth/GetMyPlaces.js
@@ -1,23 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const GetMyPlaces = (props) => {
-
-  console.log(props)
+  const [allMyPlaces, setAllMyPlaces] = useState(null)
+  
   useEffect(() => {
     const getPlaces = async() => {
       const { data } = await axios.get('/api/profile/getmyplaces')
       const myPlaces = data.filter(item => {
         return props.savedPlaces.includes(item._id)
       })
-      console.log(myPlaces)
+      setAllMyPlaces(myPlaces)
     }
     getPlaces()
   }, [])
 
+  if (!allMyPlaces) return null
 
   return (
-    <h1>Your stuff here</h1>
+    <>
+      <h1>Your stuff here</h1>
+      {allMyPlaces.map(place => {
+        return <h2 key={ place._id }>{place.nameOfDestination}</h2>
+      })}
+    </>
+    
   )
 }
 
