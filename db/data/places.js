46fb97093d9de1fbd6d60e3110c70791a3593376
kkaur89 <karenjeetkaurp@gmail.com>
2021-03-31 08:38:42 +0100

    added more dyas and palces

--- db/data/places.js
+++ db/data/places.js
@@ -1,5 +1,21 @@
 export default [
 
+  
+  {
+    nameOfDestination: 'Blue Lagoon',
+    typeOfDestination: 'Activity',
+    longitude: -22.453665530291346,
+    latitude: 63.88181365893593,
+    description: 'World-renowned spa is known for its healing waters, vivid azure colours and luxurious treatments. It is located in the middle of a moss-covered lava field, giving it an otherworldly atmosphere.',
+    image: 'https://images.unsplash.com/photo-1439725434120-c1b50e0cc329?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    icon: '🤿',
+    packageId: 1,
+    winterAccess: true,
+    day: [1],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
+
   {
     nameOfDestination: 'Eyja Guldsmeden Hotel',
     typeOfDestination: 'Hotel',
@@ -10,21 +26,6 @@ export default [
     description: 'Located in Reykjavik, Eyja Guldsmeden Hotel is in the city centre. Harpa and National Museum of Iceland are cultural highlights, or if an activity is on the agenda, check out Reykjavik Harbour. Laugardalslaug Geothermal Pool and Sundhollin are also worth visiting.',
     image: 'https://images.trvl-media.com/hotels/32000000/31010000/31000400/31000378/170a1104.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium',
     icon: '🛏',
-    packageId: 1,
-    winterAccess: true,
-    day: [1],
-    packages: [1],
-    packageName: ['Complete Ring Road of Iceland']
-  },
-
-  {
-    nameOfDestination: 'Blue Lagoon',
-    typeOfDestination: 'Activity',
-    longitude: -22.453665530291346,
-    latitude: 63.88181365893593,
-    description: 'World-renowned spa is known for its healing waters, vivid azure colours and luxurious treatments. It is located in the middle of a moss-covered lava field, giving it an otherworldly atmosphere.',
-    image: 'https://images.unsplash.com/photo-1439725434120-c1b50e0cc329?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-    icon: '🤿',
     packageId: 1,
     winterAccess: true,
     day: [1],
@@ -47,20 +48,6 @@ export default [
     packageName: ['Complete Ring Road of Iceland']
   },
 
-  {
-    nameOfDestination: 'Hotel Grimsborgir - Golden Circle Retreat',
-    typeOfDestination: 'Hotel',
-    longitude: -20.970381,
-    latitude: 64.052484,
-    description: 'Located in Selfoss, Hotel Grimsborgir – Your Golden Circle Retreat is next to a golf course and near the boardwalk. The area`s natural beauty can be seen at Kerid Crater and Thingvellir National Park. Looking to get your feet wet? Kayaking, scuba diving and snorkelling adventures can be found near the property.',
-    image: 'https://images.trvl-media.com/hotels/4000000/3650000/3644200/3644141/0f57615e.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium',
-    icon: '🛏',
-    packageId: 1,
-    winterAccess: true,
-    day: [2],
-    packages: [1],
-    packageName: ['Complete Ring Road of Iceland']
-  },
 
   {
     nameOfDestination: 'The golden Circle',
@@ -70,6 +57,20 @@ export default [
     description: 'Day two sends you off to some of the best-known natural phenomena in Iceland on the Golden Circle route. The first stop on the Golden Circle tour is Þingvellir National Park. Here, you can walk between the rift valley of the Mid-Atlantic Ridge and explore an area which played a huge role in Iceland`s heritage; its history as the site of the world`s first representative parliament has warranted its place on the UNESCO world heritage list. You can even opt into a snorkel in the crystal clear fissure Silfra, which runs right between the tectonic plates.',
     image: 'https://guidetoiceland.imgix.net/503241/x/0/in-the-geysir-geothermal-area-you-ll-feel-a-thrilling-moment-of-anticipation-right-before-the-geyser-strokkur-erupts.jpg?auto=format%2Ccompress&fit=crop&w=1446',
     icon: '💠',
+    packageId: 1,
+    winterAccess: true,
+    day: [2],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
+  {
+    nameOfDestination: 'Hotel Grimsborgir - Golden Circle Retreat',
+    typeOfDestination: 'Hotel',
+    longitude: -20.970381,
+    latitude: 64.052484,
+    description: 'Located in Selfoss, Hotel Grimsborgir – Your Golden Circle Retreat is next to a golf course and near the boardwalk. The area`s natural beauty can be seen at Kerid Crater and Thingvellir National Park. Looking to get your feet wet? Kayaking, scuba diving and snorkelling adventures can be found near the property.',
+    image: 'https://images.trvl-media.com/hotels/4000000/3650000/3644200/3644141/0f57615e.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium',
+    icon: '🛏',
     packageId: 1,
     winterAccess: true,
     day: [2],
@@ -93,6 +94,93 @@ export default [
   },
 
 
+  {
+    nameOfDestination: 'South Coast',
+    typeOfDestination: 'Activity',
+    longitude: -21.942400,
+    latitude: 64.147209,
+    description: 'On day three, you will take in the sight of two majestic waterfalls, Seljalandsfoss and Skógafoss. Further down the coast near the town of Vík, take a slight detour to admire the black volcanic beach, Reynisfjara, and the dramatic Dyrhólaey and Reynisdrangar rock formations. This is also a great place for puffin watching. Today, you have the opportunity to have a glacier adventure! You can race across Sólheimajökull glacier tongue on a thrilling snowmobile tour or, if you`d like, you can visit a natural ice cave in Mýrdalsjökull glacier. Only accessible via super jeep, this is the only cave that is open to visits all year long.On your way to your accommodations in the Vatnajökull area, you’ll pass the beautiful Jökulsárlón glacier lagoon where enormous icebergs that have broken off a nearby glacier float peacefully on the serene lagoon. Don’t worry; you will have plenty of time tomorrow to explore this area further if you are too knackered after today’s adventures.',
+    image: 'https://guidetoiceland.imgix.net/261073/x/0/the-dark-cliffs-of-hexagonal-basalt-columns-lend-svartifoss-its-name-the-black-waterfall.jpg?auto=format%2Ccompress&fit=crop&w=1446',
+    icon: '💠',
+    packageId: 4,
+    winterAccess: true,
+    day: [3],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
+
+  {
+    nameOfDestination: 'FossHotel Glacier Lagoon',
+    typeOfDestination: 'Hotel',
+    longitude: -16.65825380556029,
+    latitude: 63.93350089643769,
+    description: 'Located in Fagurholsmyri, Fosshotel Glacier Lagoon is in the mountains and in a national park. Ingolfshofdi is a notable landmark, and the area`s natural beauty can be seen at Vatnajokull and Hvannadalshnukur. Guests love the hotel`s quiet location',
+    image: 'https://images.trvl-media.com/hotels/13000000/12800000/12794300/12794262/486363de.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium',
+    icon: '🛏',
+    packageId: 1,
+    winterAccess: true,
+    day: [3],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
+  {
+    nameOfDestination: 'Glacier Goodies',
+    typeOfDestination: 'Restaurant',
+    longitude: -16.897280,
+    latitude: 63.992560,
+    description: 'Quick Bites; Seafood; Fast Food',
+    image: 'https://media-cdn.tripadvisor.com/media/photo-o/08/29/64/37/restaurant-geysir-glima.jpg',
+    icon: '🍽',
+    packageId: 1,
+    winterAccess: true,
+    day: [3],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
+
+  {
+    nameOfDestination: 'Jokulsarlon Glacier Lagoon & Eastfjords',
+    typeOfDestination: 'Activity',
+    longitude: -16.230555 ,
+    latitude: 64.078445,
+    description: 'Explore the stunning Jökulsárlón glacier lagoon today, one of Iceland’s most beautiful spots. If you so desire, you can hop on an optional boat tour, which takes you on a meandering path through the lagoon, amongst stunning icebergs of all shapes and sizes. You can choose between an amphibious boat and a smaller zodiac. Make sure you stroll down to the black sandy beach nearby, affectionately named Diamond Beach due to the number of sparkling icebergs that are scattered along it. Finally, you`ll start on a zigzagging journey along the Eastfjords, passing tranquil fishing villages and sweeping coastal landscapes along the way. Spend the night in the secluded Egilsstaðir area.',
+    image: 'https://guidetoiceland.imgix.net/519545/x/0/on-a-self-drive-tour-you-ll-have-the-freedom-to-spend-as-much-time-as-you-like-watching-the-giants-icebergs-on-jokulsarlon-glacier-lagoon.jpg?auto=format%2Ccompress&fit=crop&w=1446',
+    icon: '💠',
+    packageId: 4,
+    winterAccess: true,
+    day: [4],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
+
+  {
+    nameOfDestination: 'Icelandair Hotel Herad',
+    typeOfDestination: 'Hotel',
+    longitude: -14.405410,
+    latitude: 65.261910,
+    description: 'Icelandair Hotel Herad is located in Egilsstadir. Vök Baths and Seydisfjordur Harbour are worth checking out if an activity is on the agenda, while those wishing to experience the area`s natural beauty can explore Fardaga Waterfall and Lagarfljot. Break out the clubs and hit the links on a nearby golf course, or seek out an adventure with hiking/biking trails and mountain biking.',
+    image: 'https://images.trvl-media.com/hotels/2000000/1410000/1404800/1404761/65d2811e.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium',
+    icon: '🛏',
+    packageId: 1,
+    winterAccess: true,
+    day: [4],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
+  {
+    nameOfDestination: 'Heimahumar Local Langoustine',
+    typeOfDestination: 'Restaurant',
+    longitude: -15.209860,
+    latitude: 64.252130,
+    description: 'Quick Bites; Seafood; Fast Food',
+    image: 'https://media-cdn.tripadvisor.com/media/photo-s/17/07/4d/63/heimahumar-vegan-roll.jpg',
+    icon: '🍽',
+    packageId: 1,
+    winterAccess: true,
+    day: [4],
+    packages: [1],
+    packageName: ['Complete Ring Road of Iceland']
+  },
 
 
   {
