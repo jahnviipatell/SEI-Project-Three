e67f97689dc46a6e420b28e36f1cef3b3e97b79d
Jahnvi <jahnvi18patel@gmail.com>
2021-04-28 15:30:17 +0100

    agian

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,9 @@
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/icelander'
-const port = process.env.PORT || 4000
-const secret = process.env.SECRET || 'This is a secret'
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/icelander'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'This is a secret'
 
-export default {
-  dbURI,
-  port,
-  secret
-}
+// export default {
+//   dbURI,
+//   port,
+//   secret
+// }
