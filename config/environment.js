85949eeb2e3dc9094c4b3462aacda74a87f2c826
Jahnvi <jahnvi18patel@gmail.com>
2021-04-28 15:06:50 +0100

    export default

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/icelander'
 const port = process.env.PORT || 4000
 const secret = process.env.SECRET || 'This is a secret'
 
-module.exports = {
+export default {
   dbURI,
   port,
   secret
